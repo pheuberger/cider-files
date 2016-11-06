644a0c6e4550867cfca269d707d1b6cb4e6883b9
pheuberger <philipp.heuberger@outlook.com>
2016-11-06 14:05:11 +0100

    Add `keka` to casks.

---
+++ symlinks/fish/init.fish
@@ -0,0 +1,21 @@
+# ALIAS
+alias love="/Applications/love.app/Contents/MacOS/love"
+
+# PATHS
+export PATH="$PATH:/usr/local/mysql/bin"
+export PATH="$HOME/.rbenv/bin:$PATH"
+
+# RBENV
+export GEM_HOME=~/.gems
+
+# Git aliases
+balias gst 'git status'
+balias gl 'git log'
+balias ga 'git add'
+balias gcm 'git commit -m'
+balias gcam 'git commit -am'
+balias gp 'git push'
+balias gri 'git rebase -i'
+balias gco 'git checkout'
+
+cd ~/Development/
